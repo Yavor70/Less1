fb08871024b9b7da3b838929e43bffd26bb82f4f
Yavor70 <yavorbit@mail.ru>
2022-10-25 13:58:05 +0300

    create file Less1

--- Less1.py
+++ Less1.py
@@ -1,1 +1,30 @@
-print('Hallo')
+my_age = 25 #int
+myNomber = 1 #int
+
+the_number_pi = 3.14 #float
+sec = 60.1
+
+#вывод сообщений в консоль
+print(my_age)
+print(myNomber)
+print(the_number_pi)
+print(sec)
+
+i_was_born = "12 февраля" #str
+myYears = "1997 age" #str
+
+#вывод сообщений в консоль
+print(i_was_born)
+print(type(myYears)) #узнаем  текущий тип
+name = input("Введите имя:") #Ввод информации
+print("Привет", name)
+
+a = 10
+b = 12
+print(a<b)
+print(a==b)
+print(a!=b)
+
+
+
+
